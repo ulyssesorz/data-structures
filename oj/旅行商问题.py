286a261514720374f483df7ab9d27b001ceba41b
ulyssesorz <2284903150@qq.com>
2019-12-17 20:29:42 +0800

    oj

--- oj/旅行商问题.py
+++ oj/旅行商问题.py
@@ -3,29 +3,23 @@
 第二行输入城市间的距离矩阵。
 输出最短距离
 """
-class TSP:
-	def __init__(self,graph,start):
-		self.graph=graph    #距离矩阵
-		self.start=start    #出发的点
-
-	def solve(self,node,sets):
-		if len(sets)==0:                    #所有节点都已遍历过后，返回最后一个点到出发点的距离
-			return self.graph[node][self.start]
-		distance=[]
-		for i in range(len(sets)):
-			cur=sets[i]                     #选中一个节点
-			temp=sets[:]                    #此处不能写成temp=sets，否则两者内存地址相同，恒相等
-			temp.pop(i)                     #temp为未遍历的节点
-			distance.append(self.graph[node][cur]+self.solve(cur,temp))     #solve递归找cur和未遍历节点的最短距离
-		return min(distance)                #未遍历完所有节点时，返回此时的最小distance，即出发点到该点的最短距离
+def tsp(node,set):
+	if len(set)==0:
+		return graph[node][start]   #所有节点都已遍历过后，返回最后一个点到出发点的距离
+	distance=[]
+	for i in range(len(set)):
+		cur=set[i]              #选中一个节点
+		others=set[:]           #此处不能写成others=set，否则两者内存地址相同，恒相等
+		others.pop(i)           #others为未遍历的节点
+		distance.append(graph[node][cur]+tsp(cur,others))   #tsp递归找cur和未遍历节点的最短距离
+	return min(distance)        #未遍历完所有节点时，返回此时的最小distance，即出发点到该点的最短距离
 
 if __name__ == '__main__':
-	g=[]
+	start=0             #起始节点
+	graph=[]
 	n=int(input())
 	for i in range(n):
-		g.append(list(map(int,input().split(','))))
-
-	sets = list(range(len(g)))
-	sets.pop(sets.index(0))         # sets代表每个节点，不包括起始点
-	tsp=TSP(g,0)
-	print(tsp.solve(0,sets))
+		graph.append(list(map(int,input().split(','))))
+	set=list(range(len(graph)))
+	set.pop(set.index(start))       # set代表每个节点，不包括起始点
+	print(tsp(start,set))
